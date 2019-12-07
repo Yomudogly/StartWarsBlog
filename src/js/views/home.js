3c921592cc78566b6e95f26a761eb55be40a2d41
Roman Khalnepesov <khalnepesov@gmail.com>
2019-12-07 16:09:08 +0000

    cards layout

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,88 +9,88 @@ export const Home = () => {
 	return (
 		<div className="container">
 			<h2 className="text-center">PEOPLE</h2>
-			<ul className="list-group">
+
+			<div className="card-columns">
 				{store.people.map((item, index) => {
 					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>
-									{item.name} {item.gender}
-								</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
+						<div className="card" key={index}>
+							<img
+								className="card-img-top"
+								src="https://picsum.photos/180/100?random=5"
+								alt="Card image cap"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">{item.name}</h5>
+								<p className="card-text">
+									<span>Birth Year: {item.birth_year}</span> <br />
+									<span>Gender: {item.gender}</span> <br />
+									<span>Height: {item.height}</span> <br />
+									<span>Mass: {item.mass}</span> <br />
+									<span>Skin: {item.skin_color}</span> <br />
+									<span>Eye Color: {item.eye_color}</span>
 								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
+							</div>
+						</div>
 					);
 				})}
-			</ul>
+			</div>
+
 			<div className="mt-5 mb-5" />
 			<h2 className="text-center">PLANETS</h2>
-			<ul className="list-group">
+
+			<div className="card-columns">
 				{store.planets.map((item, index) => {
 					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>
-									{item.name} {item.population}
-								</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
+						<div className="card" key={index}>
+							<img
+								className="card-img-top"
+								src="https://picsum.photos/200/100?random=2"
+								alt="Card image cap"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">{item.name}</h5>
+								<p className="card-text">
+									<span>Population: {item.population}</span> <br />
+									<span>Climate: {item.climate}</span> <br />
+									<span>Terrain: {item.terrain}</span> <br />
+									<span>Diameter: {item.diameter}</span> <br />
+									<span>Gravity: {item.gravity}</span> <br />
+									<span>Orbital Period: {item.orbital_period}</span>
 								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
+							</div>
+						</div>
 					);
 				})}
-			</ul>
+			</div>
+
 			<div className="mt-5 mb-5" />
 			<h2 className="text-center">VEHICLES</h2>
-			<ul className="list-group">
+
+			<div className="card-columns">
 				{store.vehicles.map((item, index) => {
 					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>
-									{item.name} {item.manufacturer}
-								</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
+						<div className="card" key={index}>
+							<img
+								className="card-img-top"
+								src="https://picsum.photos/200/100?random=7"
+								alt="Card image cap"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">{item.name}</h5>
+								<p className="card-text">
+									<span>Cargo Capacity: {item.cargo_capacity}</span> <br />
+									<span>Crew: {item.crew}</span> <br />
+									<span>Passengers: {item.passengers}</span> <br />
+									<span>Manufacturer: {item.manufacturer}</span> <br />
+									<span>Consumables: {item.consumables}</span> <br />
+									<span>Cost in Credits: {item.cost_in_credits}</span>
 								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
+							</div>
+						</div>
 					);
 				})}
-			</ul>
+			</div>
+
 			<br />
 			<Link to="/">
 				<button className="btn btn-primary">Back home</button>
