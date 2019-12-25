67307c40e3b9dd1b0e49e6e9412e50e802822c7e
Roman Khalnepesov <khalnepesov@gmail.com>
2019-12-25 17:07:26 +0000

    lazy-load added to home component to speedup fetching

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { lazy } from "react";
 import { Link } from "react-router-dom";
-import HomePeople from "./home-people";
-import HomePlanets from "./home-planets";
-import HomeVehicles from "./home-vehicles";
+import loadable from "@loadable/component";
+// import HomePeople from "./home-people";
+// import HomePlanets from "./home-planets";
+// import HomeVehicles from "./home-vehicles";
 
-// const HomePeople = React.lazy(() => import("./home-people"));
-// const HomePlanets = React.lazy(() => import("./home-planets"));
-// const HomeVehicles = React.lazy(() => import("./home-vehicles"));
+const HomePeople = loadable(() => import("./home-people"));
+const HomePlanets = loadable(() => import("./home-planets"));
+const HomeVehicles = loadable(() => import("./home-vehicles"));
 
 export const Home = () => {
 	return (
